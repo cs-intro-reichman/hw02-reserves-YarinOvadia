9b670b5403afc321644f1d22f514a92be23e5674
Yarin Ovadia <yarin.ovadia@post.runi.ac.il>
2024-02-06 23:16:50 +0200

    update

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -24,6 +24,67 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
-		    
+		
+		
+		
+		int twochild = 0;
+		int threechild = 0; 
+		int fourchild = 0; 
+		double sumtot = 0.0;
+		double avg = 0.0;
+		String common ;
+
+		for ( int i = 0; i < t ; i++) {
+
+			boolean boy = false; 
+			boolean girl = false; 
+			int sum = 0;
+
+			while (boy == false || girl == false){
+				double a = generator.nextDouble();
+				if (a >= 0.5){
+					boy = true;
+
+				}
+
+				else {
+					girl = true;
+
+				}
+				sum ++;
+			}
+			sumtot += sum;
+			if (sum == 2) {
+				twochild ++;
+			}
+			else if (sum == 3){
+				threechild ++;
+			}
+			else if (sum >= 4) {
+				fourchild ++;
+			}
+			
+		
+		}
+	 
+	avg = sumtot / t;
+
+	if (twochild >= threechild && twochild >= fourchild){
+		common = "2.";
+	}
+	else if (threechild >= twochild && threechild >= fourchild){
+		common = "3.";
+	}
+	else{
+		common = "4 or more.";
+	}
+	
+
+	System.out.println("Average: " +avg+ " children to get at least one of each gender.");
+	System.out.println("Number of families with 2 children: " +twochild);
+	System.out.println("Number of families with 3 children: " +threechild);
+	System.out.println("Number of families with 4 or more children: " +fourchild);
+	System.out.println("The most common number of children is " +common);
+    
 	}
 }
